1e25fd64a16d5a88dfc35611093e055115113ce4
hagher01 <113406765+hagher01@users.noreply.github.com>
2024-09-28 18:06:06 -0400

    Update python.py

--- python.py
+++ python.py
@@ -2,4 +2,4 @@ import requests
 from discord import Webhook, RequestsWebhookAdapter
 
 webhook = Webhook.partial(1283614885661511712, 'hJ8nqs84PwRE4r5MSupsWcGy9nhSH8S73aif-Ks4sD3-jVZWT9uN0NqW0zIQ_AxSburc/github', adapter = RequestsWebhookAdapter ())
-webhook.send( 'Hello World', username='Foo'
+webhook.send( 'Hello World', username='Foo')
