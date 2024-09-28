3238e3f212794feade9b4dc15275ebfb33f80594
hagher01 <113406765+hagher01@users.noreply.github.com>
2024-09-28 18:31:41 -0400

    Update python.py

--- python.py
+++ python.py
@@ -1,15 +1,17 @@
-import asyncio import discord
-from discord import Webhook import aiohttp
-async def anything(url):
-async with aiohttp. ClientSession() as session:
-webhook = Webhook. from_url(url, session=session)
-embed = discord. Embed(titLe="This is from a webhook!")
-await webhook. send (embed=embed, • username="Richard Web")
-if _name.
-== "_main_":
-url =
-'https://discord.com/api/webhooks/1283614885661511712/hJ8nqs84PwRE4r5MSupsWcGy9nhSH8S73aif-Ks4sD3-jVZWT9uN0NqW0zIQ_AxSburc/github'
+import asyncio 
+import discord
+from discord import Webhook
+import aiohttp
 
-loop = asyncio.new_event_loop()
-loop.run_until_complete(anything(url))
-loop.close
+async def anything(url):
+   async with aiohttp. ClientSession() as session:
+     webhook = Webhook. from_url(url, session=session)
+     embed = discord. Embed(titLe="This is from a webhook!")
+     await webhook. send (embed=embed, • username="Richard Web")
+
+if _name.== "_main_":
+  url = 'https://discord.com/api/webhooks/1283614885661511712/hJ8nqs84PwRE4r5MSupsWcGy9nhSH8S73aif-Ks4sD3-jVZWT9uN0NqW0zIQ_AxSburc/github'
+
+  loop = asyncio.new_event_loop()
+  loop.run_until_complete(anything(url))
+  loop.close
