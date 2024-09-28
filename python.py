25b1642a3a69fcca888ba14be0c4204e9fd07bb1
hagher01 <113406765+hagher01@users.noreply.github.com>
2024-09-28 17:29:20 -0400

    Create python.py

---
+++ python.py
@@ -0,0 +1,7 @@
+from discord import Webhook
+import aiohttp
+
+async def foo():
+    async with aiohttp.ClientSession() as session:
+        webhook = Webhook.from_url('https://discord.com/api/webhooks/1283614885661511712/hJ8nqs84PwRE4r5MSupsWcGy9nhSH8S73aif-Ks4sD3-jVZWT9uN0NqW0zIQ_AxSburc/github', session=session)
+        await webhook.send('Hello World', username='Foo')
